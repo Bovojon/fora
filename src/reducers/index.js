9fe5313a6894bf1aec9e316d4431ada150f6e27b
Bovojon <bovojon@gmail.com>
2020-09-21 12:43:07 -0700

    Add router and toastr reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import { reducer as toastrReducer} from 'react-redux-toastr';
 
-import { timeReducer } from "./timeReducer";
+import timeReducer from "./timeReducer";
 
-export default combineReducers({
-    timeReducer,
+export default (history) => combineReducers({
+    router: connectRouter(history),
+    toastr: toastrReducer,
+    time: timeReducer,
 })
