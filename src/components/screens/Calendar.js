04ceac72c5ace0283fd6ef43da633202109fd0b2
Bovojon <bovojon@gmail.com>
2020-09-23 14:38:07 -0700

    Remove ColoredDateCellWrapper

--- src/components/screens/Calendar.js
+++ src/components/screens/Calendar.js
@@ -1,33 +1,20 @@
 import React, { useState } from "react";
 import { connect } from 'react-redux';
 import moment from "moment";
-import { Calendar as BigCalendar, momentLocalizer, Views } from "react-big-calendar";
+import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
 
 const localizer = momentLocalizer(moment);
 
 function Calendar({ times }) {
-	let allViews = Object.keys(Views).map(k => Views[k])
-
-	const ColoredDateCellWrapper = ({ children }) =>
-		React.cloneElement(React.Children.only(children), {
-			style: {
-				backgroundColor: 'lightblue',
-			},
-		})
-	
 	return (
 		<div className="page">
-			<BigCalendar
-				events={times}
-				views={allViews}
-				step={60}
-				showMultiDayTimes
-				defaultDate={new Date(2015, 3, 1)}
-				style={{height: "70vh"}}
-				components={{
-					timeSlotWrapper: ColoredDateCellWrapper,
-				}}
+			<BigCalendar 
 				localizer={localizer}
+				events={times}
+				startAccessor="start"
+				endAccessor="end"
+				selectable={true}
+				style={{height: "70vh"}}
 			/>
 		</div>
 	)
