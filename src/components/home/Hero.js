df927c052614c0e5f344e1c7322cc3ea1a9959c5
Bovojon <bovojon@gmail.com>
2020-09-25 11:08:55 -0700

    Add src/images

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -4,9 +4,8 @@ import tw from "twin.macro";
 
 import Header from "./Navbar";
 
-import { ReactComponent as SvgDecoratorBlob1 } from "../images/svg-decorator-blob-1.svg";
-import DesignIllustration from "../images/design-illustration-2.svg";
-import CustomersLogoStripImage from "../images/customers-logo-strip.png";
+import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
+import DesignIllustration from "../../images/design-illustration-2.svg";
 
 const Container = tw.div`relative`;
 const TwoColumn = tw.div`flex flex-col lg:flex-row lg:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
@@ -63,7 +62,6 @@ export default ({ roundedHeaderButton }) => {
             </Actions>
             <CustomersLogoStrip>
               <p>Our TRUSTED Customers</p>
-              <img src={CustomersLogoStripImage} alt="Our Customers" />
             </CustomersLogoStrip>
           </LeftColumn>
           <RightColumn>
