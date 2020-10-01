00f04c81631905f19a6f73244bbede54548e92a7
Bovojon <bovojon@gmail.com>
2020-10-01 14:51:21 -0700

    Show times selected on right of calendar

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { connect } from 'react-redux';
 import moment from "moment";
-import {
-	Grid,
-	Box
-} from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
+import { Card, CardTitle, CardText } from 'reactstrap';
 import { 
 	Calendar as BigCalendar, 
 	momentLocalizer,
@@ -12,31 +10,48 @@ import {
 	Navigate
 } from "react-big-calendar";
 
+import { addTime } from '../actions/time';
+
 const localizer = momentLocalizer(moment);
 
-function Calendar({ times }) {
+function Calendar({ times, addTime }) {
+	const [selectedTimes, setSelectedTimes] = useState(times);
+
+	const handleSelectSlot = (selected) => {
+		let { start, end } = selected;
+		start = new Date(start);
+		end = new Date(end);
+		const newTime = { start, end }
+		setSelectedTimes([...selectedTimes, newTime]);
+	}
+
 	return (
 		<Box alignItems="center" style={{ 'marginTop': '70px' }}>
-		<Grid container spacing={3} direction="row" alignItems="center" justify="center">
-			<Grid item xs={8}>
-				<BigCalendar
-					localizer={localizer}
-					events={times}
-					startAccessor="start"
-					endAccessor="end"
-					selectable={true}
-					defaultView={Views.WEEK}
-					views={{ month: true, week: true }}
-					style={{height: "70vh"}}
-					min={new Date(0, 0, 0, 6, 0, 0)}
-					max={new Date(0, 0, 0, 23, 0, 0)}
-					scrollToTime={new Date(0, 0, 0, 7, 0, 0)}
+			<Grid container spacing={3} direction="row" alignItems="center" justify="center">
+				<Grid item xs={8}>
+					<BigCalendar
+						localizer={localizer}
+						events={selectedTimes}
+						startAccessor="start"
+						endAccessor="end"
+						selectable
+						defaultView={Views.WEEK}
+						views={{ month: true, week: true }}
+						style={{height: "70vh"}}
+						scrollToTime={new Date(0, 0, 0, 7, 0, 0)}
+						onSelectSlot={handleSelectSlot}
 					/>
+				</Grid>
+				<Grid item xs={3}>
+					{selectedTimes.map(time => {
+						return (
+						<Card key={time.start} body outline color="primary">
+							<CardTitle>{String(time.start)}</CardTitle>
+						</Card>
+						)
+					})}
+				</Grid>
 			</Grid>
-			<Grid item xs={3}>
-				Selected:
-			</Grid>
-    </Grid>
 		</Box>
 	)
 }
@@ -45,4 +60,13 @@ const mapStateToProps = (state) => ({
 	times: state.time,
 })
 
-export default connect(mapStateToProps)(Calendar);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		addTime: (time) => {
+			console.log(time)
+			dispatch(addTime(time))
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Calendar);
