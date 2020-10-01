b32b9c0b30c7c6a53b53557add3758f059a17f2b
Bovojon <bovojon@gmail.com>
2020-10-01 14:50:21 -0700

    Remove preloadedState

--- src/store/index.js
+++ src/store/index.js
@@ -8,26 +8,8 @@ import createRootReducer from '../reducers';
 export const history = createBrowserHistory();
 const logger = createLogger();
 
-const preloadedState = {
-  time: [
-    {
-      id: 0,
-      title: 'All Day Event very long title',
-      start: new Date("2020-09-03"),
-      end: new Date("2020-09-03"),
-    },
-    {
-      id: 1,
-      title: 'Long Event',
-      start: new Date("2020-09-25"),
-      end: new Date("2020-09-25"),
-    },
-  ]
-}
-
 const store = createStore(
   createRootReducer(history),
-  preloadedState,
   compose(
     applyMiddleware(
       routerMiddleware(history),
