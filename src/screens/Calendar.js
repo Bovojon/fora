dc4b71c2e77d7fdd6260995bf591ce3c828d529d
Bovojon <bovojon@gmail.com>
2020-10-03 20:39:43 -0700

    Redesign the times listed

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -3,13 +3,12 @@ import { connect } from 'react-redux';
 import moment from "moment";
 import styled from 'styled-components'
 import { Calendar as BigCalendar, momentLocalizer, Views } from "react-big-calendar";
-import { Grid, Box } from '@material-ui/core';
+import { Grid, Box as MuiBox, Paper as MuiPaper } from '@material-ui/core';
 import { Clear as ClearIcon } from '@material-ui/icons';
 import { 
-	Card as rCard,
-	CardText,
-	CardTitle as rCardTitle,
-	CardBody as rCardBody,
+	Card as ReactCard,
+	CardTitle as ReactCardTitle,
+	CardBody as ReactCardBody,
 	Row
 } from 'reactstrap';
 
@@ -17,19 +16,35 @@ import { addTime, deleteTime } from '../actions/time';
 
 const localizer = momentLocalizer(moment);
 
-const Card = styled(rCard)`
+const Card = styled(ReactCard)`
 	padding: 0.7rem;
 	margin-bottom: 5px;
 `;
 
-const CardBody = styled(rCardBody)`
+const CardBody = styled(ReactCardBody)`
 	padding: 0
 `;
 
-const CardTitle = styled(rCardTitle)`
+const CardTitle = styled(ReactCardTitle)`
 	margin-left: 4px;
 	margin-bottom: 4px;
 	cursor: pointer;
+`
+
+const Paper = styled(MuiPaper)`
+	height: 65vh;
+	overflow: auto;
+	margin-top: 5vh;
+	padding: 14px;
+`
+
+const ShortPaper = styled(MuiPaper)`
+	padding: 14px;
+	height: 10vh;
+`
+
+const Box = styled(MuiBox)`
+	margin-top: 50px;
 `
 
 function Calendar({ times, addTime, deleteTime }) {
@@ -47,7 +62,7 @@ function Calendar({ times, addTime, deleteTime }) {
 	}
 
 	return (
-		<Box alignItems="center" style={{ 'marginTop': '70px' }}>
+		<Box alignItems="center">
 			<Grid container spacing={3} direction="row" alignItems="center" justify="center">
 				<Grid item xs={8}>
 					<BigCalendar
@@ -56,31 +71,41 @@ function Calendar({ times, addTime, deleteTime }) {
 						startAccessor="start"
 						endAccessor="end"
 						selectable
+						style={{height: "70vh"}}
 						defaultView={Views.WEEK}
 						views={{ month: true, week: true }}
-						style={{height: "70vh"}}
 						scrollToTime={new Date(0, 0, 0, 7, 0, 0)}
 						onSelectSlot={handleSelectSlot}
 					/>
 				</Grid>
 				<Grid item xs={3}>
-					{times.map(time => {
-						return (
-						<Card key={time.id} body outline color="primary">
-							<CardBody>
-								<CardTitle onClick={() => handleDelete(time.id)} className="float-right"> <ClearIcon /> </CardTitle>
-								<CardText>
-									<Row>
-										{moment(time.start).format('ddd, MMM Do')}
-									</Row>
-									<Row>
-										{moment(time.start).format('hh:mm a') + " – " + moment(time.end).format('hh:mm a')} 
-									</Row>
-								</CardText>
-							</CardBody>
-						</Card>
-						)
-					})}
+					{
+						times.length === 0 ? 
+						<ShortPaper>
+							<MuiBox justifyContent="center" alignItems="center" display="flex">
+								<MuiBox p={2} style={{ fontSize: '18px' }}>
+									Pick the times that work for you.
+								</MuiBox>
+							</MuiBox>
+						</ShortPaper>
+						:
+						<Paper variant="outlined">
+							{times.map(time => {
+								return (
+								<Card key={time.id} body outline color="primary">
+									<CardBody>
+										<CardTitle onClick={() => handleDelete(time.id)} className="float-right"> <ClearIcon /> </CardTitle>
+										<Row>
+											{moment(time.start).format('ddd, MMM Do')}
+										</Row>
+										<Row>
+											{moment(time.start).format('h:mm a') + " – " + moment(time.end).format('h:mm a')} 
+										</Row>
+									</CardBody>
+								</Card>
+							)})}
+						</Paper>
+					}
 				</Grid>
 			</Grid>
 		</Box>
