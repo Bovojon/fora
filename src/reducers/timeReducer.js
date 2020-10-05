05534d0138dc75917d3c1a3344be4b6e1459d316
Bovojon <bovojon@gmail.com>
2020-10-04 23:03:22 -0700

    Check if the same user selects the same time

--- src/reducers/timeReducer.js
+++ src/reducers/timeReducer.js
@@ -10,11 +10,25 @@ const timeSorter = (a, b) => {
 const timeReducer = (state=[], { type, payload }) => {
   switch(type) {
     case ADD_TIME:
-      const newTime = {
-        id: ++nextId,
-        ...payload
+      /**
+       * Do not allow if the same user adds the same time.
+       * Allow if 2 different users add the same time.
+       */
+      const { start, end } = payload;
+      const alreadyExists = state.filter(time => 
+        (
+          moment(time.start).format('X') === moment(start).format('X') && 
+          moment(time.end).format('X') === moment(end).format('X')
+        )
+      );
+      if (alreadyExists.length === 0) {
+        const newTime = {
+          id: ++nextId,
+          ...payload
+        }
+        return [...state, newTime].sort(timeSorter);
       }
-      return [...state, newTime].sort(timeSorter);
+      return state
     case DELETE_TIME:
       return state.filter(time => time.id !== payload)
     default:
