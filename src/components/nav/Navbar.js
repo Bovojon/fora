2acb8cbb27856cab0ee368f0a6f8e497378d65a9
Bovojon <bovojon@gmail.com>
2020-10-28 13:48:31 -0700

    Improve design of dhare modal

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -4,6 +4,10 @@ import { push } from 'connected-react-router';
 import { useLocation } from 'react-router-dom';
 import tw from "twin.macro";
 import styled from "styled-components";
+import { 
+  Clear as ClearIcon, 
+  FilterNoneOutlined as CopyIcon 
+} from '@material-ui/icons';
 import {
   Navbar,
   NavbarBrand as BootNavbarBrand,
@@ -12,7 +16,13 @@ import {
   Nav,
   NavItem
 } from 'reactstrap';
-import { Modal, Backdrop, Fade, Box as MuiBox, makeStyles } from '@material-ui/core';
+import { 
+  Grid,
+  Modal, 
+  Backdrop, 
+  Fade,
+  makeStyles
+} from '@material-ui/core';
 
 const NavbarBrand = styled(BootNavbarBrand)`
   ${tw`text-2xl! text-4xl font-black`};
@@ -24,28 +34,52 @@ const NavLinks = styled.div`
   a { ${tw`no-underline`} }
 `
 
-const NavLink = tw.a`
-  text-lg my-2 lg:mx-6 lg:my-0 font-semibold tracking-wide transition duration-300
-  pb-1 border-b-2 border-transparent hover:border-blue-500 hocus:text-blue-500 no-underline
-`;
-
-const PrimaryLink = tw.a`
-  lg:mx-0 px-4 py-3 rounded bg-blue-500 text-gray-100 ml-3 hover:bg-blue-700 hover:text-gray-200 
-  hover:shadow-outline border-b-0 font-semibold
-`;
-
-const Box = styled(MuiBox)`
-  padding: 24px 24px 20px 24px;
-  border-radius: 8px;
-  background-color: #fff;
+const PrimaryLink = styled.a`
+  ${tw`lg:mx-0 px-4 py-3 rounded bg-blue-500 ml-3 hover:bg-blue-700 
+    hover:shadow-outline border-b-0 font-semibold cursor-pointer`}
+  color: white !important;
 `
 
-const SmallTitle = styled.p`
-  font: 500 20px Roboto,RobotoDraft,Helvetica,Arial,sans-serif;
+const ModalContent = styled(Grid)`
+  padding: 15px 24px 20px 24px;
+  border-radius: 8px;
+  background-color: #fff;
+  max-width: 400px;
+  min-height: 200px;
+  min-width: 400px;
+`
+
+const SmallTitle = styled.span`
   font-size: 16px;
   font-weight: 500;
   letter-spacing: .1px;
   line-height: 24px;
+  padding-bottom: 15px;
+`
+
+const LightText = styled.span`
+  color: #5f6368;
+  font: 400 14px / 20px Roboto, sans-serif;
+  margin-bottom: 12px;
+`
+
+const CopyArea = styled(Grid)`
+  align-items: center;
+  background: #f1f3f4;
+  border-radius: 4px;
+  color: #5f6368;
+  display: flex;
+  padding: 15px 12px;
+`
+
+const LinkText = styled.span`
+  letter-spacing: .00625em;
+  font-family: Roboto,Arial,sans-serif;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+  color: #202124;
+  flex-grow: 1;
 `
 
 const useStyles = makeStyles((theme) => ({
@@ -90,13 +124,13 @@ const MainNavbar = ({ navigateTo }) => {
             {location.pathname === "/calendar" ?
               <NavItem>
                 <NavLinks>
-                  <PrimaryLink css="rounded-full" href="#" onClick={handleShareClick}>Share calendar</PrimaryLink>
+                  <PrimaryLink css="rounded-full" onClick={handleShareClick}>Share calendar</PrimaryLink>
                 </NavLinks>
               </NavItem>
               :
               <NavItem>
                 <NavLinks>
-                  <PrimaryLink onClick={handleFindTimeClick} css="rounded-full" href="/">Find a time</PrimaryLink>
+                  <PrimaryLink onClick={handleFindTimeClick} css="rounded-full">Find a time</PrimaryLink>
                 </NavLinks>
               </NavItem>
             }
@@ -112,9 +146,14 @@ const MainNavbar = ({ navigateTo }) => {
         BackdropComponent={Backdrop}
         BackdropProps={{ timeout: 500 }}>
         <Fade in={modalIsOpen}>
-          <Box boxShadow={5}>
+          <ModalContent boxShadow={5} container direction="column" justify="space-evenly" alignItems="flex-start">
             <SmallTitle>Here's the link to the calendar</SmallTitle>
-          </Box>
+            <LightText>Also, here is the calendar ID if you just want to share that: </LightText>
+            <CopyArea container direction="row" justify="space-between" alignItems="center">
+              <LinkText>fora.com/calendar/link</LinkText>
+              <CopyIcon />
+            </CopyArea>
+          </ModalContent>
         </Fade>
       </Modal>
     </>
