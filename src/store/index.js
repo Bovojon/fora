7cfc335c9911ed4a5628c0b7fb176b38b9326658
Bovojon <bovojon@gmail.com>
2020-11-01 13:05:27 -0800

    Add sagaMiddleware to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddle from 'redux-saga';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import { createLogger } from 'redux-logger';
 
-import createRootReducer from '../reducers';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
+const sagaMiddleware = createSagaMiddle();
 export const history = createBrowserHistory();
 const logger = createLogger();
 
-const store = createStore(
-  createRootReducer(history),
-  compose(
-    applyMiddleware(
-      routerMiddleware(history),
-      logger,
-    )
-  )
-)
+const middlewares = applyMiddleware(
+  sagaMiddleware,
+  routerMiddleware(history),
+  logger
+);
+
+const store = createStore(rootReducer(history), compose(middlewares));
+
+sagaMiddleware.run(rootSaga)
 
 export default store;
