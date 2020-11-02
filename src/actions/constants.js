faced27a94b5ddbcbdddaaa19a244b1270d9eedb
Bovojon <bovojon@gmail.com>
2020-11-02 15:32:13 -0800

    Add more action constants

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,2 +1,21 @@
+/**
+ * Calendar
+ */
+export const CREATE_CALENDAR = "CREATE_CALENDAR";
+export const CREATE_CALENDAR_SUCCESS = "CREATE_CALENDAR_SUCCESS";
+export const CREATE_CALENDAR_ERROR = "CREATE_CALENDAR_ERROR";
+
+export const GET_CALENDAR = "GET_CALENDAR";
+export const GET_CALENDAR_SUCCESS = "GET_CALENDAR_SUCCESS";
+
+/**
+ * Time
+ */
 export const ADD_TIME = "ADD_TIME";
 export const DELETE_TIME = "DELETE_TIME";
+
+/**
+ * User
+ */
+export const POST_USER = "POST_USER";
+export const POST_USER_SUCCESS = "POST_USER_SUCCESS";
