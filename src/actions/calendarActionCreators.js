46105a925c878f2bd3a3b1bb3e0df39d0f37e719
Bovojon <bovojon@gmail.com>
2020-11-03 15:06:16 -0800

    Small fix to constant naming

--- src/actions/calendarActionCreators.js
+++ src/actions/calendarActionCreators.js
@@ -5,14 +5,13 @@ import {
   CALENDAR_CREATED_SUCCESS,
   CALENDAR_CREATED_ERROR,
 
-  CALENDAR_FETCHED, 
-  CALENDAR_FETCHED_SUCCESS,
-  CALENDAR_FETCHED_PENDING
+  CALENDAR_FETCHED_PENDING, 
+  CALENDAR_FETCHED_SUCCESS
 } from './constants';
 
 export const createCalendarCreator = createAction(CALENDAR_CREATED_PENDING);
 export const createCalendarSuccessCreator = createAction(CALENDAR_CREATED_SUCCESS);
 export const createCalendarErrorCreator = createAction(CALENDAR_CREATED_ERROR);
 
-export const getCalendarCreator = createAction(CALENDAR_FETCHED);
+export const getCalendarCreator = createAction(CALENDAR_FETCHED_PENDING);
 export const putCalendarCreator = createAction(CALENDAR_FETCHED_SUCCESS);
