59aa74d3a6e9f079c82e34fa0f530a932ef79538
Bovojon <bovojon@gmail.com>
2020-11-05 19:57:26 -0800

    Add setCalendarOwner() to CalendarService

--- src/services/calendarService.js
+++ src/services/calendarService.js
@@ -2,7 +2,9 @@ import AxiosAPI from './apiService';
 
 const CalendarService = {
   createCalendar: () => { return AxiosAPI.post('/calendar/create') },
-  getCalendar: (data) => { return AxiosAPI.post('/calendar/get', data) }
+  getCalendar: (data) => { return AxiosAPI.post('/calendar/get', data) },
+  addUserToCalendar: (data) => { return AxiosAPI.post('/calendar/addUser', data) },
+  setCalendarOwner: (data) => { return AxiosAPI.post('/calendar/setOwner', data) }
 }
 
 export default CalendarService;
