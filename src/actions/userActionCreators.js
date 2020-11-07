a68ef47fa371ec73a5b0a485c3718b5bcfa17d84
Bovojon <bovojon@gmail.com>
2020-11-06 16:15:26 -0800

    Add createUserSuccess()

--- src/actions/userActionCreators.js
+++ src/actions/userActionCreators.js
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { USER_CREATED_PENDING } from './constants';
+import { USER_CREATED_PENDING, USER_CREATED_SUCCESS } from './constants';
 
 export const createUserPending = createAction(USER_CREATED_PENDING);
+export const createUserSuccess = createAction(USER_CREATED_SUCCESS);
