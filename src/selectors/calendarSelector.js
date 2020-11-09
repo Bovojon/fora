486fdce28c694dd7a80c383ff70ebff6d716545a
Bovojon <bovojon@gmail.com>
2020-11-08 20:19:47 -0800

    Add getCurrentUserId and getCalendarId selectors

--- src/selectors/calendarSelector.js
+++ src/selectors/calendarSelector.js
@@ -1,5 +1,6 @@
 const CalendarSelector = {
-  getTotalParticipants: (state) => { return state.calendar.participants.length }
+  getTotalParticipants: (state) => { return state.calendar.participants.length },
+  getCalendarId: (state) => { return state.calendar.id }
 }
 
 export default CalendarSelector;
--- src/selectors/userSelector.js
+++ src/selectors/userSelector.js
@@ -1,5 +1,6 @@
 const UserSelector = {
-  getCurrentUser: (state) => { return state.user }
+  getCurrentUser: (state) => { return state.user },
+  getCurrentUserId: (state) => { return state.user.id }
 }
 
 export default UserSelector;
