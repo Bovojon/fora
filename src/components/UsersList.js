48dbc747434257834d47659c8dbf4116585de8b4
Bovojon <bovojon@gmail.com>
2020-11-09 01:13:23 -0800

    Clean up Skeleton code for UsersList

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { Skeleton } from '@material-ui/lab';
 import { 
   List,  
   ListItem,
@@ -22,6 +23,37 @@ const Header = styled.span`
   color: #4299e1;
 `
 
+const LoadingListSkeleton = () => {
+  return (
+    <List>
+      <ListItem>
+        <ListItemAvatar>
+          <Skeleton animation="wave" variant="circle" width={40} height={40} />
+        </ListItemAvatar>
+        <ListItemText>
+          <Skeleton animation="wave" variant="text" height={20} width="90%" />
+        </ListItemText>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Skeleton animation="wave" variant="circle" width={40} height={40} />
+        </ListItemAvatar>
+        <ListItemText>
+          <Skeleton animation="wave" variant="text" height={20} width="90%" />
+        </ListItemText>
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Skeleton animation="wave" variant="circle" width={40} height={40} />
+        </ListItemAvatar>
+        <ListItemText>
+          <Skeleton animation="wave" variant="text" height={20} width="90%" />
+        </ListItemText>
+      </ListItem>
+    </List>
+  );
+}
+
 const UsersList = ({ participants }) => {
   const [checked, setChecked] = useState([]);
   const [isLoading, setIsLoading] = useState(typeof participants === "undefined");
@@ -29,20 +61,17 @@ const UsersList = ({ participants }) => {
   const handleCheckBoxClick = (labelId) => () => {
     const currentIndex = checked.indexOf(labelId);
     const newChecked = [...checked];
-
     if (currentIndex === -1) {
       newChecked.push(labelId);
     } else {
       newChecked.splice(currentIndex, 1);
     }
-
     setChecked(newChecked);
   };
 
   useEffect(() => {
     const fetchProduct = () => {
       if (typeof participants === "undefined") {
-        console.log("-------------------undefineddddd")
         setIsLoading(true)
       } else {
         setIsLoading(false)
@@ -54,12 +83,11 @@ const UsersList = ({ participants }) => {
   return (
     <Box my={2}>
       <Header><h4>Others in this calendar:</h4></Header>
-      <List>
-        {
-          isLoading ? 
-          "Loading"
-          :
-          participants.map((participant) => {
+      {isLoading ? 
+        <LoadingListSkeleton />
+        :
+        <List>
+          {participants.map((participant) => {
             const labelId = participant.id;
             return (
               <ListItem key={labelId} button>
@@ -72,40 +100,11 @@ const UsersList = ({ participants }) => {
                 </ListItemSecondaryAction>
               </ListItem>
             );
-          })
-        }
-      </List>
+          })}
+        </List>
+      }
     </Box>
   );
 }
-
-
-// {
-//   "id": 1,
-//   "unique_id": null,
-//   "name": null,
-//   "email": null,
-//   "phone_number": null,
-//   "color": null,
-//   "avatar": null,
-//   "created_at": "2020-11-09T06:25:12.114Z",
-//   "updated_at": "2020-11-09T06:25:12.114Z",
-//   "deleted_at": null
-// }
-
-// {participants.map((participant) => {
-//   const labelId = participant.id;
-//   return (
-//     <ListItem key={labelId} button>
-//       <ListItemAvatar>
-//         <Avatar/>
-//       </ListItemAvatar>
-//       <ListItemText id={labelId} primary={participant.name} />
-//       <ListItemSecondaryAction>
-//         <Checkbox edge="end" onChange={handleCheckBoxClick(labelId)} checked={checked.indexOf(labelId) !== -1} inputProps={{ 'aria-labelledby': labelId }} />
-//       </ListItemSecondaryAction>
-//     </ListItem>
-//   );
-// })}
 
 export default UsersList;
