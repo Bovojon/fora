96796b23b8c1704bb9fbde6226dcf75063084023
Bovojon <bovojon@gmail.com>
2020-11-11 11:41:37 -0800

    Check for loading and empty times array in TimesList

--- src/components/TimesList.js
+++ src/components/TimesList.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import styled from 'styled-components';
 import { Clear as ClearIcon } from '@material-ui/icons';
-import { Box as MuiBox } from '@material-ui/core';
+import { Box as MuiBox, Grid } from '@material-ui/core';
+import { Skeleton as MuiSkeleton } from '@material-ui/lab';
 import { 
 	Card as ReactCard,
 	CardTitle as ReactCardTitle,
@@ -29,7 +30,7 @@ const Box = styled(MuiBox)`
   height: 41vh;
 `
 
-const ListArea = styled(MuiBox)`
+const ListArea = styled(Grid)`
   height: 36vh;
   overflow: auto;
   margin-top: 15px;
@@ -41,44 +42,66 @@ const Header = styled.span`
   color: #4299e1;
 `
 
+const Skeleton = styled(MuiSkeleton)`
+  margin-top: 15px;
+`
+
+const LoadingListSkeleton = () => {
+  return (
+    <ListArea container direction="column" justify="flex-start" alignItems="center">
+      <Skeleton variant="rect" width="90%" height="10%" />
+      <Skeleton variant="rect" width="90%" height="10%" />
+      <Skeleton variant="rect" width="90%" height="10%" />
+    </ListArea>
+  );
+}
+
 const TimesList = ({ times, handleDelete, currentUser }) => {
+  const [isLoading, setIsLoading] = useState(typeof times === "undefined");
+
   return (
     <Box mt={4}>
-      {times.length === 0 ? 
-        <Header><h4>Select times that work for you.</h4></Header>
+      {isLoading ?
+        <LoadingListSkeleton />
         :
         <>
-          <Header><h4>Your selected times:</h4></Header>
-          <ListArea>
-            {times.map(time => {
-              if (moment(time.start).format('YYYY-MM-DD') !== moment(time.end).format('YYYY-MM-DD')){
-                return (
-                  <Card key={time.id} body outline color="primary">
-                    <CardBody>
-                      <CardTitle onClick={() => handleDelete(time.id)} className="float-right"> <ClearIcon /> </CardTitle>
-                      <Row>
-                        {moment(time.start).format('MMM D') + " – " + moment(time.end).format('MMM D')}
-                      </Row>
-                    </CardBody>
-                  </Card>
-                );
-              } else {
-                return (
-                  <Card key={time.id} body outline color="primary">
-                    <CardBody>
-                      <CardTitle onClick={() => handleDelete(time.id)} className="float-right"> <ClearIcon color="action" /> </CardTitle>
-                      <Row>
-                        {moment(time.start).format('ddd, MMM D')}
-                      </Row>
-                      <Row>
-                        {moment(time.start).format('h:mm A') + " – " + moment(time.end).format('h:mm A')} 
-                      </Row>
-                    </CardBody>
-                  </Card>
-                );
-              }
-            })}
-          </ListArea>
+          {times.length === 0 ?
+            <Header><h4>Selected times will be listed here.</h4></Header>
+            :
+            <>
+              <Header><h4>Selected times:</h4></Header>
+              <ListArea>
+                {times.map(time => {
+                  if (moment(time.start).format('YYYY-MM-DD') !== moment(time.end).format('YYYY-MM-DD')){
+                    return (
+                      <Card key={time.id} body outline color="primary">
+                        <CardBody>
+                          <CardTitle onClick={() => handleDelete(time.id)} className="float-right"> <ClearIcon /> </CardTitle>
+                          <Row>
+                            {moment(time.start).format('MMM D') + " – " + moment(time.end).format('MMM D')}
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    );
+                  } else {
+                    return (
+                      <Card key={time.id} body outline color="primary">
+                        <CardBody>
+                          <CardTitle onClick={() => handleDelete(time.id)} className="float-right"> <ClearIcon color="action" /> </CardTitle>
+                          <Row>
+                            {moment(time.start).format('ddd, MMM D')}
+                          </Row>
+                          <Row>
+                            {moment(time.start).format('h:mm A') + " – " + moment(time.end).format('h:mm A')} 
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    );
+                  }
+                })}
+              </ListArea>
+            </>
+          }
         </>
       }
     </Box>
