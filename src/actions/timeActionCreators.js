1f5cb64bef46f57e793190cba5480420f42c68eb
Bovojon <bovojon@gmail.com>
2020-11-10 18:17:49 -0800

    Add time action creators and 1 new TIME_DUPLICATE_ERROR constant

--- src/actions/timeActionCreators.js
+++ src/actions/timeActionCreators.js
@@ -1,9 +1,15 @@
 import { createAction } from '@reduxjs/toolkit';
 
 import { 
-  TIME_ADDED_PENDING, 
-  TIME_REMOVED_PENDING 
+  TIME_ADDED_PENDING,
+  TIME_ADDED_SUCCESS,
+  
+  TIME_REMOVED_PENDING,
+  TIME_REMOVED_SUCCESS
 } from './constants';
 
 export const addTimePending = createAction(TIME_ADDED_PENDING);
+export const addTimeSuccess = createAction(TIME_ADDED_SUCCESS);
+
 export const removeTimePending = createAction(TIME_REMOVED_PENDING);
+export const removeTimeSuccess = createAction(TIME_REMOVED_SUCCESS);
