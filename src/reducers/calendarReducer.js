4036c454e8123f8fd5da15d3c82e2aebed6a9ce6
Bovojon <bovojon@gmail.com>
2020-11-12 00:56:42 -0800

    Update store on CALENDAR_FETCHED_SUCCESS

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -1,8 +1,13 @@
-import { CALENDAR_CREATED_SUCCESS, CALENDAR_ADDED_USER_SUCCESS } from "../actions/constants";
+import { 
+  CALENDAR_CREATED_SUCCESS, 
+  CALENDAR_ADDED_USER_SUCCESS,
+  CALENDAR_FETCHED_SUCCESS
+} from "../actions/constants";
 
 const calendarReducer = (state={}, { type, payload }) => {
   switch(type) {
     case CALENDAR_CREATED_SUCCESS:
+    case CALENDAR_FETCHED_SUCCESS:
     case CALENDAR_ADDED_USER_SUCCESS:
       const calendarObj = payload;
       return {...state, ...calendarObj}
