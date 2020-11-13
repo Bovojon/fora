b654dbf87a3a4c8e7f70cbdd4fe6488561c7dc84
Bovojon <bovojon@gmail.com>
2020-11-12 20:41:58 -0800

    Calendar created_pending and created_success same as fetched reducer cases

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -1,5 +1,6 @@
 import { 
   CALENDAR_FETCHED_PENDING,
+  CALENDAR_CREATED_PENDING,
   CALENDAR_CREATED_SUCCESS, 
   CALENDAR_ADDED_USER_SUCCESS,
   CALENDAR_FETCHED_SUCCESS
@@ -11,16 +12,17 @@ const initialCalendarObj = {
 
 const calendarReducer = (state=initialCalendarObj, { type, payload }) => {
   switch(type) {
+    case CALENDAR_CREATED_PENDING:
     case CALENDAR_FETCHED_PENDING: {
       const status = { isLoading: true }
       return {...state, status}
     }
+    case CALENDAR_CREATED_SUCCESS:
     case CALENDAR_FETCHED_SUCCESS: {
       const calendarObj = payload;
       calendarObj.status = { isLoading: false };
       return {...state, ...calendarObj}
     }
-    case CALENDAR_CREATED_SUCCESS:
     case CALENDAR_ADDED_USER_SUCCESS: {
       const calendarObj = payload;
       return {...state, ...calendarObj}
