f4cf375f0ddfd04a705a7f6c0668f99c105abf22
Bovojon <bovojon@gmail.com>
2020-11-12 16:43:54 -0800

    Set scopes for cases in CalendarReducer and add state obj to it

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -1,16 +1,39 @@
 import { 
+  CALENDAR_FETCHED_PENDING,
   CALENDAR_CREATED_SUCCESS, 
   CALENDAR_ADDED_USER_SUCCESS,
   CALENDAR_FETCHED_SUCCESS
 } from "../actions/constants";
 
-const calendarReducer = (state={}, { type, payload }) => {
+const initialState = {
+  status: {
+    isLoading: false,
+    error: null,
+  }
+}
+
+const calendarReducer = (state=initialState, { type, payload }) => {
   switch(type) {
+    case CALENDAR_FETCHED_PENDING: {
+      const status = {
+        isLoading: true,
+        error: null,
+      }
+      return {...state, status}
+    }
+    case CALENDAR_FETCHED_SUCCESS: {
+      let calendarObj = payload;
+      calendarObj.status = {
+        isLoading: false,
+        error: null,
+      }
+      return {...state, ...calendarObj}
+    }
     case CALENDAR_CREATED_SUCCESS:
-    case CALENDAR_FETCHED_SUCCESS:
-    case CALENDAR_ADDED_USER_SUCCESS:
+    case CALENDAR_ADDED_USER_SUCCESS: {
       const calendarObj = payload;
       return {...state, ...calendarObj}
+    }
     default:
       return state
   }
