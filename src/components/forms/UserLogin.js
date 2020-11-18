73741f770fb7f0c460693043135b9792c0e55e96
Bovojon <bovojon@gmail.com>
2020-11-18 02:44:55 -0800

    Change paddings of Dialog title and list inside

--- src/components/forms/UserLogin.js
+++ src/components/forms/UserLogin.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from "styled-components";
 import { 
 	Avatar as MuiAvatar, 
-	List,
+	List as MuiList,
   ListItem,
   ListItemAvatar,
   ListItemText,
-  DialogTitle,
+  DialogTitle as MuiDialogTitle,
   Dialog,
 } from '@material-ui/core';
 import {
@@ -17,6 +17,14 @@ import {
 const Avatar = styled(MuiAvatar)`
   background-color: #bbdefb;
   color: #1e88e5;
+`
+
+const DialogTitle = styled(MuiDialogTitle)`
+  padding: 16px 24px 5px 24px;
+`
+
+const List = styled(MuiList)`
+  padding: 0px 24px 16px 24px;
 `
 
 const UserLogin = ({ dialogIsOpen, handleDialogClose, fullScreen, selectedAccount, emails }) => {
@@ -39,7 +47,7 @@ const UserLogin = ({ dialogIsOpen, handleDialogClose, fullScreen, selectedAccoun
           </ListItem>
         ))}
 
-        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+        <ListItem button onClick={() => handleListItemClick('addAccount')}>
           <ListItemAvatar>
             <Avatar>
               <AddIcon />
