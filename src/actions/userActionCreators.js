5d810f37d787c3b68dc739dd2f9d899aeb4702ad
bovojon <bovojon@gmail.com>
2020-11-22 13:10:20 -0800

    Add USER_UPDATED_PENDING and USER_UPDATED_SUCCESS

--- src/actions/userActionCreators.js
+++ src/actions/userActionCreators.js
@@ -1,8 +1,18 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { USER_CREATED_PENDING, USER_CREATED_SUCCESS, USER_SET_CURRENT_PENDING } from './constants';
+import { 
+  USER_CREATED_PENDING, 
+  USER_CREATED_SUCCESS, 
+  USER_SET_CURRENT_PENDING,
+
+  USER_UPDATED_PENDING,
+  USER_UPDATED_SUCCESS
+} from './constants';
 
 export const createUserPending = createAction(USER_CREATED_PENDING);
 export const createUserSuccess = createAction(USER_CREATED_SUCCESS);
+
+export const updateUserPending = createAction(USER_UPDATED_PENDING);
+export const updateUserSuccess = createAction(USER_UPDATED_SUCCESS);
 
 export const setCurrentUserPending = createAction(USER_SET_CURRENT_PENDING);
