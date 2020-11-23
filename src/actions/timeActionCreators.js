ce66f71920ceacee10db32f203cbf6c42aac1d74
bovojon <bovojon@gmail.com>
2020-11-22 20:03:51 -0800

    Add TIMES_FILTERED action and creator

--- src/actions/timeActionCreators.js
+++ src/actions/timeActionCreators.js
@@ -8,7 +8,9 @@ import {
   TIME_REMOVED_SUCCESS,
 
   TIMES_FETCHED_PENDING,
-  TIMES_FETCHED_SUCCESS
+  TIMES_FETCHED_SUCCESS,
+
+  TIMES_FILTERED
 } from './constants';
 
 export const addTimePending = createAction(TIME_ADDED_PENDING);
@@ -19,3 +21,5 @@ export const removeTimeSuccess = createAction(TIME_REMOVED_SUCCESS);
 
 export const fetchTimesPending = createAction(TIMES_FETCHED_PENDING);
 export const fetchTimesSuccess = createAction(TIMES_FETCHED_SUCCESS);
+
+export const filterTimes = createAction(TIMES_FILTERED);
