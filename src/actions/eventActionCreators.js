a7db80b28fd15c842c0702d4d4dd211bb8c2849a
bovojon <bovojon@gmail.com>
2020-12-23 03:09:54 +0500

    Add EVENT_SUBMITTED_PENDING

--- src/actions/eventActionCreators.js
+++ src/actions/eventActionCreators.js
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { EVENT_ADDED_PENDING } from './constants';
+import { EVENT_ADDED_PENDING, EVENT_SUBMITTED_PENDING } from './constants';
 
 export const addEventPending = createAction(EVENT_ADDED_PENDING);
+export const submitEventPending = createAction(EVENT_SUBMITTED_PENDING);
