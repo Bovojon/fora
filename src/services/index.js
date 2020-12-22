19ad2835fc638b799221ad96d6638116e13b8e97
bovojon <bovojon@gmail.com>
2020-12-23 03:10:39 +0500

    Add eventService

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 import UserService from './userService';
 import CalendarService from './calendarService';
 import TimeService from './timeService';
+import EventService from './eventService.js';
 
 export {
   UserService,
   CalendarService,
-  TimeService
+  TimeService,
+  EventService
 }
