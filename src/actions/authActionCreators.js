0f6588c215a9f6bc9373694ddea56202e93df6f5
bovojon <bovojon@gmail.com>
2020-12-23 22:42:42 +0500

    AUTH_CODE_REMOVE_SUCCESS

--- src/actions/authActionCreators.js
+++ src/actions/authActionCreators.js
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { AUTH_CODE_ADDED_SUCCESS } from './constants';
+import { AUTH_CODE_ADDED_SUCCESS, AUTH_CODE_REMOVE_SUCCESS } from './constants';
 
 export const addAuthCodeSuccess = createAction(AUTH_CODE_ADDED_SUCCESS);
+export const removeAuthCodeSuccess = createAction(AUTH_CODE_REMOVE_SUCCESS);
