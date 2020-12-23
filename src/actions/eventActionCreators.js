5f70085983a55881e8d6cdd2c4be890e7f29e5e0
bovojon <bovojon@gmail.com>
2020-12-23 18:08:22 +0500

    Add EVENT_SUBMITTED_SUCCESS and EVENT_SUBMITTED_ERROR

--- src/actions/eventActionCreators.js
+++ src/actions/eventActionCreators.js
@@ -1,6 +1,14 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { EVENT_ADDED_PENDING, EVENT_SUBMITTED_PENDING } from './constants';
+import {
+  EVENT_ADDED_PENDING,
+  EVENT_SUBMITTED_PENDING,
+  EVENT_SUBMITTED_SUCCESS,
+  EVENT_SUBMITTED_ERROR
+} from './constants';
 
 export const addEventPending = createAction(EVENT_ADDED_PENDING);
+
 export const submitEventPending = createAction(EVENT_SUBMITTED_PENDING);
+export const submitEventSuccess = createAction(EVENT_SUBMITTED_SUCCESS);
+export const submitEventError = createAction(EVENT_SUBMITTED_ERROR);
