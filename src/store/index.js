e533c6f0b3d2809ec6959ecd9bb1e17e7c1afd2a
bovojon <bovojon@gmail.com>
2021-01-08 00:30:25 +0500

    Remove redux-logger for prod

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddle from 'redux-saga';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
-import { createLogger } from 'redux-logger';
 
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
@@ -10,13 +9,11 @@ import customMiddlewares from '../middlewares';
 
 const sagaMiddleware = createSagaMiddle();
 export const history = createBrowserHistory();
-const logger = createLogger();
 
 const middlewares = applyMiddleware(
   ...customMiddlewares,
   sagaMiddleware,
-  routerMiddleware(history),
-  logger
+  routerMiddleware(history)
 );
 
 const store = createStore(rootReducer(history), compose(middlewares));
