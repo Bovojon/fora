3fe78688234c06f00a1dbd35a46341464796d826
bovojon <bovojon@gmail.com>
2021-01-12 16:07:59 +0500

    Add importEventsPending and importEventsSuccess

--- src/actions/eventActionCreators.js
+++ src/actions/eventActionCreators.js
@@ -4,7 +4,9 @@ import {
   EVENT_ADDED_PENDING,
   EVENT_SUBMITTED_PENDING,
   EVENT_SUBMITTED_SUCCESS,
-  EVENT_SUBMITTED_ERROR
+  EVENT_SUBMITTED_ERROR,
+  EVENTS_IMPORTED_PENDING,
+  EVENTS_IMPORTED_SUCCESS
 } from './constants';
 
 export const addEventPending = createAction(EVENT_ADDED_PENDING);
@@ -12,3 +14,6 @@ export const addEventPending = createAction(EVENT_ADDED_PENDING);
 export const submitEventPending = createAction(EVENT_SUBMITTED_PENDING);
 export const submitEventSuccess = createAction(EVENT_SUBMITTED_SUCCESS);
 export const submitEventError = createAction(EVENT_SUBMITTED_ERROR);
+
+export const importEventsPending = createAction(EVENTS_IMPORTED_PENDING);
+export const importEventsSuccess = createAction(EVENTS_IMPORTED_SUCCESS);
