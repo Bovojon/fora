0c66df9f2c9b14d89fa2fc0883d2dee0f90b0130
bovojon <bovojon@gmail.com>
2021-01-12 16:08:48 +0500

    Add fetchAuthClientPending and fetchAuthClientSuccess

--- src/actions/authActionCreators.js
+++ src/actions/authActionCreators.js
@@ -1,6 +1,14 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { AUTH_CODE_ADDED_SUCCESS, AUTH_CODE_REMOVE_SUCCESS } from './constants';
+import {
+  AUTH_CODE_ADDED_SUCCESS,
+  AUTH_CODE_REMOVED_SUCCESS,
+  AUTH_CLIENT_FETCHED_PENDING,
+  AUTH_CLIENT_FETCHED_SUCCESS
+} from './constants';
 
 export const addAuthCodeSuccess = createAction(AUTH_CODE_ADDED_SUCCESS);
-export const removeAuthCodeSuccess = createAction(AUTH_CODE_REMOVE_SUCCESS);
+export const removeAuthCodeSuccess = createAction(AUTH_CODE_REMOVED_SUCCESS);
+
+export const fetchAuthClientPending = createAction(AUTH_CLIENT_FETCHED_PENDING);
+export const fetchAuthClientSuccess = createAction(AUTH_CLIENT_FETCHED_SUCCESS);
