18d752f111e625110f3755a83ac022167498cc10
bovojon <bovojon@gmail.com>
2021-01-13 12:55:03 +0500

    Undo Add getAuthClient() in eventService for now

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,10 +1,7 @@
 import AxiosAPI from './apiService';
 
 const EventService = {
-  getUrl: () => { return AxiosAPI.post('/auth/google/getUrl') },
-  getAccessToken: (data) => { return AxiosAPI.post('/auth/google/getAccessToken', data) },
-  submitEvent: (data) => { return AxiosAPI.post('/auth/google/insert', data) },
-  importEvents: (data) => { return AxiosAPI.post('/auth/google/list', data) }
+  submitEvent: (data) => { return AxiosAPI.post('/auth/google/insert', data) }
 }
 
 export default EventService;
