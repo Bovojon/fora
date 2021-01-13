b931f37815b3a2281af20351189e7fe32caa9ef2
bovojon <bovojon@gmail.com>
2021-01-14 01:10:49 +0500

    Add imported events to store

--- src/components/screens/LoadingScreens/LoadingAuth.js
+++ src/components/screens/LoadingScreens/LoadingAuth.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 import { useLocation } from "react-router-dom";
 
 import { addAuthCodeSuccess } from '../../../actions/authActionCreators';
-import { fetchCalendarSuccess } from '../../../actions/calendarActionCreators';
+import { fetchCalendarSuccess, importCalendarSuccess } from '../../../actions/calendarActionCreators';
 import { setCurrentUserPending } from '../../../actions/userActionCreators';
 import { addEventPending } from '../../../actions/eventActionCreators';
 import '../../animations/styles/loading.scss';
 
 const useQuery = () => { return new URLSearchParams(useLocation().search) }
 
-const LoadingAuth = ({ navigateTo, addAuthCode, addCalendar, setCurrentUser, addEvent }) => {
+const LoadingAuth = ({ navigateTo, addAuthCode, addCalendar, setCurrentUser, addEvent, addImportedEvents }) => {
   const code = useQuery().get("code");
   const localStorageContent = JSON.parse(localStorage.getItem('fora'));
   const { calendar, currentUser, redirectUrl } = localStorageContent;
@@ -25,7 +25,7 @@ const LoadingAuth = ({ navigateTo, addAuthCode, addCalendar, setCurrentUser, add
       url: `${process.env.REACT_APP_URL}/auth/google/list`,
       data: { ...calendarDetails, code }
 		}).then(response => {
-			console.log(response.data.events);
+      addImportedEvents(response.data.events.data.items)
 		}).catch(err =>{
 			console.error(err);
 		});
@@ -66,7 +66,8 @@ const mapDispatchToProps = (dispatch) => {
     addAuthCode: (code) => { dispatch(addAuthCodeSuccess(code)) },
     addCalendar: (calendarObj) => { dispatch(fetchCalendarSuccess(calendarObj)) },
     setCurrentUser: (userObj) => { dispatch(setCurrentUserPending(userObj)) },
-    addEvent: (eventObj) => { dispatch(addEventPending(eventObj)) }
+    addEvent: (eventObj) => { dispatch(addEventPending(eventObj)) },
+    addImportedEvents: (events) => { dispatch(importCalendarSuccess(events)) }
   }
 }
 
