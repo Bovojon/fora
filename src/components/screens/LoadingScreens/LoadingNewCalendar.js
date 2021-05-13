0112422826011e0f137e1290d41eb4f501c8f966
bovojon <bovojon@gmail.com>
2021-05-13 17:36:02 +0300

    Add loading message

--- src/components/screens/LoadingScreens/LoadingNewCalendar.js
+++ src/components/screens/LoadingScreens/LoadingNewCalendar.js
@@ -1,8 +1,19 @@
 import React, { useEffect } from "react";
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
+import anime from 'animejs/lib/anime.es.js';
+import { Grid } from '@material-ui/core';
+import styled from 'styled-components';
 
 import '../../animations/styles/loading.scss';
+
+const LoadingMessage = styled.div`
+  position: absolute;
+  top: 65%;
+  text-align: center;
+`
+
+const LOADING_MESSAGE = "Getting calendar ready...";
 
 const LoadingNewCalendar = ({ calendar, navigateTo }) => {
 	useEffect(() => {
@@ -10,7 +21,48 @@ const LoadingNewCalendar = ({ calendar, navigateTo }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [calendar]);
 
-	return (<div className="loader"></div>)
+  const addSpanAroundLetters = (str) => {
+    const arr = [];
+    for (let i = 0; i < str.length; i++) {
+      const c = str.charAt(i);
+      if (c === " ") {
+        arr.push(<span key={i}>{'   '}</span>)
+      } else {
+        arr.push(<span key={i} className='letter'>{str.charAt(i)}</span>)
+      }
+    }
+    return arr
+  }
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml6 .letter',
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 1200,
+      delay: (el, i) => 80 * i
+    }).add({
+      targets: '.ml6',
+      opacity: 0,
+      duration: 2000,
+      easing: "easeOutExpo",
+      delay: 1500
+    });
+
+	return (
+    <Grid container direction="column" justify="center" alignItems="center">
+      <div className="loader"></div>
+      <LoadingMessage>
+        <h1 className="ml6">
+          <span className="text-wrapper">
+            <span className="letters">
+              {addSpanAroundLetters(LOADING_MESSAGE)}
+            </span>
+          </span>
+        </h1>
+      </LoadingMessage>
+    </Grid>
+  )
 }
 
 const mapStateToProps = (state) => {
