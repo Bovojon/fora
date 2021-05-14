c7a1e1e9723cbcbf2604300291e14ee2c86ae173
bovojon <bovojon@gmail.com>
2021-05-14 13:51:47 +0300

    Switch animejs to CSS keyframes

--- src/components/screens/LoadingScreens/LoadingNewCalendar.js
+++ src/components/screens/LoadingScreens/LoadingNewCalendar.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
-import anime from 'animejs/lib/anime.es.js';
 import { Grid } from '@material-ui/core';
 import styled from 'styled-components';
 
@@ -13,52 +12,18 @@ const LoadingMessage = styled.div`
   text-align: center;
 `
 
-const LOADING_MESSAGE = "Getting calendar ready...";
-
 const LoadingNewCalendar = ({ calendar, navigateTo }) => {
 	useEffect(() => {
     if (calendar.status.isLoading === false) navigateTo(`/${calendar.unique_id}`);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [calendar]);
 
-  const addSpanAroundLetters = (str) => {
-    const arr = [];
-    for (let i = 0; i < str.length; i++) {
-      const c = str.charAt(i);
-      if (c === " ") {
-        arr.push(<span key={i}>{'   '}</span>)
-      } else {
-        arr.push(<span key={i} className='letter'>{str.charAt(i)}</span>)
-      }
-    }
-    return arr
-  }
-
-  anime.timeline({loop: true})
-    .add({
-      targets: '.ml6 .letter',
-      translateY: ["1.1em", 0],
-      translateZ: 0,
-      duration: 1200,
-      delay: (el, i) => 80 * i
-    }).add({
-      targets: '.ml6',
-      opacity: 0,
-      duration: 2000,
-      easing: "easeOutExpo",
-      delay: 1500
-    });
-
 	return (
     <Grid container direction="column" justify="center" alignItems="center">
       <div className="loader"></div>
       <LoadingMessage>
         <h1 className="ml6">
-          <span className="text-wrapper">
-            <span className="letters">
-              {addSpanAroundLetters(LOADING_MESSAGE)}
-            </span>
-          </span>
+          <span className="textAnimation">Getting calendar ready...</span>
         </h1>
       </LoadingMessage>
     </Grid>
