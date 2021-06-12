e0ef6509a2d09fe063de385b5ea3f467c529a762
bovojon <bovojon@gmail.com>
2021-06-12 23:17:44 +0500

    Add TIMES_GROUP_BY_USER_PENDING

--- src/actions/timeActionCreators.js
+++ src/actions/timeActionCreators.js
@@ -1,14 +1,16 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { 
+import {
   TIME_ADDED_PENDING,
   TIME_ADDED_SUCCESS,
-  
+
   TIME_REMOVED_PENDING,
   TIME_REMOVED_SUCCESS,
 
   TIMES_FETCHED_PENDING,
-  TIMES_FETCHED_SUCCESS
+  TIMES_FETCHED_SUCCESS,
+
+  TIMES_GROUP_BY_USER_PENDING
 } from './constants';
 
 export const addTimePending = createAction(TIME_ADDED_PENDING);
@@ -19,3 +21,5 @@ export const removeTimeSuccess = createAction(TIME_REMOVED_SUCCESS);
 
 export const fetchTimesPending = createAction(TIMES_FETCHED_PENDING);
 export const fetchTimesSuccess = createAction(TIMES_FETCHED_SUCCESS);
+
+export const groupTimesPending = createAction(TIMES_GROUP_BY_USER_PENDING);
