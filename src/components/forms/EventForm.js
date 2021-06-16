27cb52244d508225eb70db876420b931cceb9af6
bovojon <bovojon@gmail.com>
2021-06-17 00:27:08 +0500

    New EventForm design completed

--- src/components/forms/EventForm.js
+++ src/components/forms/EventForm.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import moment from "moment";
 import { connect } from 'react-redux';
-import { TextField, Grid } from '@material-ui/core';
+import { push } from 'connected-react-router';
 import DatePicker from "react-datepicker";
 import momentTimezone from "moment-timezone";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
+import {
+  TextField,
+  Grid,
+  Button as MuiButton,
+  CircularProgress
+} from '@material-ui/core';
 
 import { addEventPending } from '../../actions/eventActionCreators';
+import { submitEventPending } from '../../actions/eventActionCreators';
+import { addError } from '../../actions/errorActionCreators';
 
 const Container = tw.div`relative`;
 const Content = styled.div`
@@ -35,16 +44,38 @@ const Header = styled.h2`
   margin-bottom: 20px;
 `
 
-const EventForm = ({ event, participants, addEvent }) => {
+const Button = styled(MuiButton)`
+  font-size: 1rem;
+  border-radius: 9999px;
+  background-color: #fddede;
+  color: #4299e1 !important;
+  :hover {
+    background-color: #fddede;
+  }
+  border-radius: 9999px;
+  padding: 0.8rem 1.1rem;
+  width: 100%;
+  margin-top: 10px;
+  :focus {
+    outline: none;
+  }
+`
+
+const startBeforeEnd = (start, end) => {
+  return moment(end).diff(moment(start)) > 0;
+}
+
+const EventForm = ({ eventObj, participants, addEvent, code, submitEvent, addError, calendarUniqueId, navigateTo }) => {
   const [summary, setSummary] = useState("");
-  const [startDateTime, setStartDateTime] = useState(new Date(event.details.start));
-  const [endDateTime, setEndDateTime] = useState(new Date(event.details.end));
+  const [startDateTime, setStartDateTime] = useState(new Date(eventObj.details.start));
+  const [endDateTime, setEndDateTime] = useState(new Date(eventObj.details.end));
   const [attendeesList, setAttendeesList] = useState([]);
   const [attendeesStr, setAttendeesStr] = useState("");
+  const [scheduleEventLoading, setScheduleEventLoading] = useState(false);
 
   useEffect(() => {
     const tizn = momentTimezone.tz.guess();
-    const eventObj = {
+    const newEventObj = {
       details: {
         summary,
         start: { dateTime: startDateTime, timeZone: tizn },
@@ -52,7 +83,7 @@ const EventForm = ({ event, participants, addEvent }) => {
         attendees: attendeesList
       }
     }
-    addEvent(eventObj);
+    addEvent(newEventObj);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [summary, startDateTime, endDateTime, attendeesStr]);
 
@@ -72,7 +103,7 @@ const EventForm = ({ event, participants, addEvent }) => {
     }
   }, [participants]);
 
-  const handleDialogClose = () => {console.log("yes")}
+  const handleCancel = () => { navigateTo(`/${calendarUniqueId}`) }
   const handleSummaryChange = (event) => { setSummary(event.target.value) }
   const handleAttendeesChange = (event) => {
     const emails = event.target.value;
@@ -83,6 +114,16 @@ const EventForm = ({ event, participants, addEvent }) => {
     });
     setAttendeesList(attendeesList);
   }
+  const handleScheduleClick = () => {
+    setScheduleEventLoading(true);
+    const event = eventObj.details;
+    if (startBeforeEnd(event.start.dateTime, event.end.dateTime)) {
+      submitEvent({ event, code });
+    } else {
+      const errorMessage = "Please select a start time that is before the end time."
+      addError(errorMessage);
+    }
+  };
 
   return (
     <Container>
@@ -117,7 +158,11 @@ const EventForm = ({ event, participants, addEvent }) => {
             />
           </Time>
           <TextField value={attendeesStr} onChange={handleAttendeesChange} placeholder="Add guests (e.g. ex1@email.com, ex2@email.com)"
-            multiline rows={3} fullWidth margin="normal" variant="outlined" style={{ marginTop: "16px" }}/>
+            multiline rows={3} fullWidth margin="normal" variant="outlined" style={{ margin: "16px 0px" }}/>
+          <Button onClick={handleScheduleClick} disabled={scheduleEventLoading} variant="contained" disableElevation>
+            { scheduleEventLoading ? <CircularProgress size={24} /> : "Schedule event" }
+          </Button>
+          <Button onClick={handleCancel} variant="contained" disableElevation>Cancel</Button>
         </Grid>
       </Content>
     </Container>
@@ -126,14 +171,19 @@ const EventForm = ({ event, participants, addEvent }) => {
 
 const mapStateToProps = (state) => {
   return {
-    event: state.event,
-    participants: state.calendar?.participants
+    eventObj: state.event,
+    calendarUniqueId: state.calendar.unique_id,
+    participants: state.calendar?.participants,
+    code: state.auth.code
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addEvent: (eventObj) => { dispatch(addEventPending(eventObj)) }
+    addEvent: (eventObj) => { dispatch(addEventPending(eventObj)) },
+    submitEvent: (eventCodeObj) => { dispatch(submitEventPending(eventCodeObj)) },
+    addError: (errorMessage) => { dispatch(addError(errorMessage)) },
+    navigateTo: (route) => { dispatch(push(route)) }
   }
 }
 
